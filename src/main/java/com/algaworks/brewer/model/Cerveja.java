871749dbebbde3efe0e442741123099432766e32
npi-fabio-franca <fabio.franca@npisistemas.com.br>
2021-03-06 17:48:02 -0300

    Projeto Brewer 3.7

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -6,7 +6,6 @@ public class Cerveja {
 	
 	@NotBlank
 	private String sku;
-	private String nome;
 	
 	public String getSku() {
 		return sku;
@@ -14,10 +13,5 @@ public class Cerveja {
 	public void setSku(String sku) {
 		this.sku = sku;
 	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+	
 }
